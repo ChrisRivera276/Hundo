af8aa0b9f6f67b6bbebfca5bb9471b95505d617a
ChrisRivera276 <c.rivera07@outlook.com>
2022-04-11 11:13:00 -0400

    comit

--- JS/site.js
+++ JS/site.js
@@ -1,19 +1,31 @@
 // Step One - controller accept request
-function getMessage() {
-    let msg = document.getElementById("txtmessage").value;
-    displayMessage(msg);
+function getValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    displayValues(startValue, endValue);
 }
 // Final Step - view
-function displayMessage(message) {
+function displayValues(start, end) {
 
     //fist get ol element from page
     element = document.getElementById("results");
-    //next create new li element
-    let item = document.createElement("li");
-    //add classes to li element
-    item.classList.add("list-group-item");
-    // set message for li element
-    item.innerHTML = message;
-    // add new item to list
-    element.appendChild(item);
+
+    for (let i = start; i <= end; i++) {
+
+
+        //next create new div element
+        let item = document.createElement("div");
+
+        if (i % 2 == 0) {
+            item.classList.add("boldItem");
+        }
+
+        //add classes to div element
+        item.classList.add("col");
+        // set message for div element
+        item.innerHTML = i;
+        // add new item to list
+        element.appendChild(item);
+    }
 }
